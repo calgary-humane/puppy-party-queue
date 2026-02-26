3294352680c9eac22cac79911f2ee683515ebb49
calgary-humane <calem.davis@calgaryhumane.ca>
2026-02-26 02:57:18 -0700

    Update import path for supabase in QueueApp

--- components/QueueApp.tsx
+++ components/QueueApp.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { supabase } from "@/lib/supabaseBrowser";
+import { supabase } from "../lib/supabaseBrowser";
 import {
   DndContext,
   DragEndEvent,
